587a695d1c1bf30ddaa5f77170bcc7102c1cf737
gerard-moledo <gerard.moledo@gmail.com>
2024-11-30 22:25:07 -0500

    Initial screen setup

---
+++ core/src/main/java/com/gmoledo/mpve/GameScreen.java
@@ -0,0 +1,19 @@
+package com.gmoledo.mpve;
+
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.Controllers;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class GameScreen extends ScreenAdapter {
+    Controller controller;
+
+    GameScreen() {
+        controller = Controllers.getCurrent();
+    }
+
+    public void render(float delta) {
+        ScreenUtils.clear(Color.RED);
+    }
+}
--- core/src/main/java/com/gmoledo/mpve/Main.java
+++ core/src/main/java/com/gmoledo/mpve/Main.java
@@ -1,7 +1,21 @@
 package com.gmoledo.mpve;
 
-class Main {
-    static void main(String[] args) {
-        return;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.FPSLogger;
+
+public class Main extends Game {
+    FPSLogger fps_logger;
+
+    @Override
+    public void create() {
+        fps_logger = new FPSLogger();
+
+        this.setScreen(new GameScreen());
+    }
+
+    public void render() {
+        super.render();
+
+        fps_logger.log();
     }
 }
