d897c1db15728284880243684c0e3de170b2988f
gerard-moledo <gerard.moledo@gmail.com>
2024-12-02 05:05:54 -0500

    Quick Fixes Removed unnecessary ShapeRenderer usage Isolated update and drawing routines in main loop

--- core/src/main/java/com/gmoledo/mpve/GameScreen.java
+++ core/src/main/java/com/gmoledo/mpve/GameScreen.java
@@ -3,7 +3,6 @@ package com.gmoledo.mpve;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.util.Map;
@@ -12,37 +11,38 @@ public class GameScreen extends ScreenAdapter {
     Player player;
     Player opponent;
 
-    ShapeRenderer renderer;
-
     GameScreen() {
         Board.Instantiate(5);
+        // Controller initialization my fail if no controller is connected
         boolean success = Player.Initialize_Controller(Controllers.getCurrent());
         if (!success) System.out.println("No controller connected.");
 
         player = new Player(Cell.Type.player, Troop.Shape.single);
         opponent = new Player(Cell.Type.opponent, Troop.Shape.single);
-
-        renderer = new ShapeRenderer();
     }
 
+    // Main loop
     public void render(float delta) {
+        update(delta);
+        draw();
+    }
+
+    public void update(float delta) {
         player.update(delta);
         opponent.update(delta);
 
+        // Reset per-frame inputs
         for (Map.Entry<Integer, Player.Button> button : Player.buttons.entrySet()) {
             button.getValue().pressed = false;
             button.getValue().released = false;
         }
+    }
 
+    public void draw() {
         ScreenUtils.clear(Color.BLACK);
-
-        renderer.setAutoShapeType(true);
-        renderer.begin();
 
         Board.draw();
         player.draw();
         opponent.draw();
-
-        renderer.end();
     }
 }
